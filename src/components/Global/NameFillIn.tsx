a3d3303cab5c0f885a5c9f77c8ebd6dc39dd3531
jyun1 <jyuny12345@gmail.com>
2021-07-24 00:00:43 +0800

    set NameFillIn as a component

---
+++ src/components/Global/NameFillIn.tsx
@@ -0,0 +1,105 @@
+import React from "react";
+import { useRecoilValue } from "recoil";
+
+import Input from "components/Global/Input";
+import Button from "components/Global/Button";
+
+import useUserName from "util/hook/useUserName";
+import { themeState } from 'store/theme';
+import styled from "styled-components";
+import { color } from "style/theme";
+
+const themeData = {
+	light: {
+		fg: color.$title_font_color,
+		name_fg: color.$title_font_color,
+		border: color.$title_font_color,
+		button_color: color.$pink_color,
+	},
+	dark: {
+		fg: color.$dark_default_font_color,
+		name_fg: color.$fluorescent_pink_color,
+		border: color.$dark_border_color,
+		button_color: color.$fluorescent_pink_color,
+	},
+}
+
+const NameForm = styled.form`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+
+	p {
+		font-size: 22px;
+		letter-spacing: 3px;
+		margin: 0 0 25px 0;
+		transition: .3s all;
+		color: ${({ theme }) => theme.fg};
+	}
+
+	.name_space{
+		padding: 5px;
+		font-size: 22px;
+		line-height: 25px;
+		letter-spacing: 2px;
+		color: ${({ theme }) => theme.name_fg};
+		border-bottom: 2px solid ${({ theme }) => theme.border};
+	}
+
+	.warn_message {
+		margin-top: 10px;
+		color: ${({ theme }) => theme.name_fg};
+		opacity: 0.6;
+		letter-spacing: 1px;
+	}
+
+	.enter_button {
+		margin-top: 20px;
+	}
+`;
+
+interface NameFillInProperty {
+	onEnter: () => void;
+}
+
+const NameFillIn = (props: NameFillInProperty) => {
+	const { onEnter } = props;
+	const [
+		{ userName, warnMessage },
+		{ setUserName, validateUserName, setWarnMessage }
+	] = useUserName();
+
+	const theme = useRecoilValue(themeState);
+	const handleButtonClick = (e: React.MouseEvent<HTMLElement>) => {
+		e.preventDefault();
+		const isValid = validateUserName();
+
+		if (isValid) {
+			onEnter();
+		}
+	};
+
+	return (
+		<NameForm theme={themeData[theme]} id="name" className="user_input">
+			<p>請輸入名字</p>
+			<Input
+				className="name_space"
+				onFocus={() => setWarnMessage('')}
+				maxLength={8}
+				onChange={(e) => setUserName(e.target.value)}
+				value={userName}
+			/>
+			{warnMessage && <span className="warn_message">{warnMessage}</span>}
+			<Button
+				className="enter_button"
+				color={themeData[theme].button_color}
+				onClick={handleButtonClick}
+				type="submit"
+			>
+				加入遊戲
+			</Button>
+		</NameForm>
+	);
+};
+
+export default NameFillIn
--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -4,13 +4,11 @@ import db from "database";
 
 import Logo from "components/Home/Logo";
 import RoomDialog from "components/Home/RoomDialog";
-import Input from "components/Global/Input";
-import Button from "components/Global/Button";
+import NameFillIn from "components/Global/NameFillIn";
 import ThemeToggler from 'components/Global/ThemeToggler';
 
 import { RoomList, FirebaseRoomsData } from 'types/room';
 
-import useUserName from "util/hook/useUserName";
 import useInitData from "util/hook/useInitData";
 import { themeState } from 'store/theme';
 import styled from "styled-components";
@@ -19,97 +17,11 @@ import { color } from "style/theme";
 const themeData = {
 	light: {
 		bg: color.$theme_background,
-		fg: color.$title_font_color,
-		name_fg: color.$title_font_color,
-		border: color.$title_font_color,
-		button_color: color.$pink_color,
 	},
 	dark: {
 		bg: color.$dark_bg_color,
-		fg: color.$dark_default_font_color,
-		name_fg: color.$fluorescent_pink_color,
-		border: color.$dark_border_color,
-		button_color: color.$fluorescent_pink_color,
 	},
 }
-
-const NameForm = styled.form`
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-
-	p {
-		font-size: 22px;
-		letter-spacing: 3px;
-		margin: 0 0 25px 0;
-		transition: .3s all;
-		color: ${({ theme }) => theme.fg};
-	}
-
-	.name_space{
-		padding: 5px;
-		font-size: 22px;
-		line-height: 25px;
-		letter-spacing: 2px;
-		color: ${({ theme }) => theme.name_fg};
-		border-bottom: 2px solid ${({ theme }) => theme.border};
-	}
-
-	.warn_message {
-		margin-top: 10px;
-		color: ${({ theme }) => theme.name_fg};
-		opacity: 0.6;
-		letter-spacing: 1px;
-	}
-
-	.enter_button {
-		margin-top: 20px;
-	}
-`;
-
-interface NameFillInProperty {
-	openRoomList: () => void;
-}
-
-const NameFillIn = (props: NameFillInProperty) => {
-	const { openRoomList } = props;
-	const [
-		{ userName, warnMessage },
-		{ setUserName, validateUserName, setWarnMessage }
-	] = useUserName();
-
-	const theme = useRecoilValue(themeState);
-	const handleButtonClick = (e: React.MouseEvent<HTMLElement>) => {
-		e.preventDefault();
-		const isValid = validateUserName();
-
-		if (isValid) {
-			openRoomList();
-		}
-	};
-
-	return (
-		<NameForm theme={themeData[theme]} id="name" className="user_input">
-			<p>請輸入名字</p>
-			<Input
-				className="name_space"
-				onFocus={() => setWarnMessage('')}
-				maxLength={8}
-				onChange={(e) => setUserName(e.target.value)}
-				value={userName}
-			/>
-			{warnMessage && <span className="warn_message">{warnMessage}</span>}
-			<Button
-				className="enter_button"
-				color={themeData[theme].button_color}
-				onClick={handleButtonClick}
-				type="submit"
-			>
-				加入遊戲
-			</Button>
-		</NameForm>
-	);
-};
 
 const HomePage = styled.div`
 	display: flex;
@@ -176,7 +88,7 @@ const Home = () => {
 		<HomePage theme={themeData[theme]}>
 			<ThemeToggler className="on_page" />
 			<Logo className="logo" />
-			<NameFillIn openRoomList={() => toggleRoomDialog(true)} />
+			<NameFillIn onEnter={() => toggleRoomDialog(true)} />
 			<RoomDialog
 				active={showRoomDialog}
 				roomList={roomList}

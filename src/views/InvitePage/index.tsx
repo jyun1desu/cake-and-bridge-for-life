01b7b9929381aef1537690ea4dd301be34b996c9
jyun1 <jyuny12345@gmail.com>
2021-07-25 13:44:52 +0800

    feat: add dark theme of invite page

--- src/views/InvitePage/index.tsx
+++ src/views/InvitePage/index.tsx
@@ -1,19 +1,60 @@
 import React, { useEffect, useState } from 'react';
 import db from "database";
+import { useRecoilValue } from 'recoil';
 import styled from 'styled-components';
 import { RouteComponentProps } from "react-router-dom";
 import { useRecoilState } from 'recoil';
 import NameFillIn from 'components/Global/NameFillIn';
 import { FirebaseRoom } from 'types/room';
+import { themeState } from 'store/theme';
 import { userRoomState } from 'store/user';
+import { color } from 'style/theme'
+import { ThemeTypes } from 'types/theme';
 
 type Params = { roomId: string };
 
-const Page = styled.div`
+const themeData = {
+    light: {
+        stripes_1: '#f7cbca',
+        stripes_2: '#faf7f0',
+        content_bg: '#faf8ece8',
+        decoration_arrows: '#f7cbca',
+        title_block_1: color.$highlight_color,
+        title_block_2: color.$green_color,
+        title_block_3: color.$orange_color,
+        title_fg: 'white',
+        amount_fg: color.$default_font_color,
+    },
+    dark: {
+        stripes_1: '#151615',
+        stripes_2: '#1F211F',
+        content_bg: color.$dark_dim_bg_color,
+        decoration_arrows: color.$fluorescent_yellow_color,
+        title_block_1: color.$fluorescent_orange_color,
+        title_block_2: color.$fluorescent_green_color,
+        title_block_3: color.$fluorescent_yellow_color,
+        title_fg: color.$default_font_color,
+        amount_fg: color.$dark_default_font_color,
+    },
+}
+
+interface PageProperty {
+    themeType: ThemeTypes;
+}
+
+const Page = styled.div<PageProperty>`
     display: flex;
     align-items: center;
     justify-content: center;
-    background-image: linear-gradient(39deg, #f7cbca 22.22%, #faf7f0 22.22%, #faf7f0 50%, #f7cbca 50%, #f7cbca 72.22%, #faf7f0 72.22%, #faf7f0 100%);
+    background-image: 
+        linear-gradient(39deg, 
+            ${({theme})=>theme.stripes_1} 22.22%, 
+            ${({theme})=>theme.stripes_2} 22.22%, 
+            ${({theme})=>theme.stripes_2} 50%, 
+            ${({theme})=>theme.stripes_1} 50%, 
+            ${({theme})=>theme.stripes_1} 72.22%, 
+            ${({theme})=>theme.stripes_2} 72.22%, 
+            ${({theme})=>theme.stripes_2} 100%);
     background-size: 42.90px 34.74px;
 
     .content {
@@ -23,7 +64,8 @@ const Page = styled.div`
         align-items: center;
         padding: 40px 0 20px;
         box-sizing: border-box;
-        background-color: #faf8ece8;
+        background-color: ${({theme})=>theme.content_bg};
+        border: 1px solid ${color.$fluorescent_pink_color};
         border-radius: 10px;
         position: relative;
     }
@@ -36,27 +78,27 @@ const Page = styled.div`
         span {
             display: inline-block;
             position: relative;
-            color: white;
+            color: ${({theme})=>theme.title_fg};
 
             &:first-child {
-                font-size: 18px;
+                font-size: 16px;
                 padding: 3px;
-                background-color: pink;
+                background-color: ${({theme})=>theme.title_block_1};
                 transform: rotate(-1deg);
             }
 
             &:nth-child(2) {
                 z-index: 10;
-                font-size: 24px;
+                font-size: 26px;
                 padding: 5px;
-                background-color: green;
+                background-color: ${({theme})=>theme.title_block_2};
                 transform: rotate(-4deg);
             }
     
             &:last-child {
                 font-size: 18px;
                 padding: 2px;
-                background-color: orange;
+                background-color: ${({theme})=>theme.title_block_3};
                 transform: rotate(3deg);
             }
         }
@@ -68,17 +110,19 @@ const Page = styled.div`
         span {
             display: block;
             transform: rotate(90deg);
-            color: #f7cbca;
+            color: ${({theme})=>theme.decoration_arrows};
         }
     }
 
     .players_amount {
         font-size: 13px;
         margin-top: 10px;
+        color: ${({theme})=>theme.amount_fg};
     }
 `
 const InvitePage = ({ match }: RouteComponentProps<Params>) => {
     const roomId = match.params.roomId;
+    const theme = useRecoilValue(themeState);
     const [roomName, setRoomName] = useRecoilState(userRoomState);
     const [currentPlayer, setPlayers] = useState<string[]>([]);
 
@@ -88,7 +132,7 @@ const InvitePage = ({ match }: RouteComponentProps<Params>) => {
             roomRef.on('value', d => {
                 const roomData = d.val() as FirebaseRoom;
                 const currentPlayers = Object.values(roomData.playersInfo).map(p => p.name)
-                setPlayers([...currentPlayers, 'jyunyi'])
+                setPlayers([...currentPlayers])
                 setRoomName(roomData.roomName);
             })
         }
@@ -101,7 +145,11 @@ const InvitePage = ({ match }: RouteComponentProps<Params>) => {
     }
 
     return (
-        <Page className="invite_page">
+        <Page 
+            theme={themeData[theme]}
+            themeType={theme}
+            className="invite_page"
+        >
             <section className="content">
                 <div className="invite_content">
                     <span>來自</span>
@@ -120,7 +168,7 @@ const InvitePage = ({ match }: RouteComponentProps<Params>) => {
                 />
                 <p className="players_amount">🍰 {
                     currentPlayer.length > 1
-                        ? `${currentPlayer[0]} 跟其他 ${currentPlayer.length - 1} 位玩家`
+                        ? `${currentPlayer[0]} 跟其他 ${currentPlayer.length - 1} 位橋牌友`
                         : currentPlayer[0]
                 }已經加入 🍰</p>
             </section>

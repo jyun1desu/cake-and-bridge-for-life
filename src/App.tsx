e832abd122c203dfd48ff60c0122ab0977cd0c8c
jyun1 <jyuny12345@gmail.com>
2021-07-29 11:42:34 +0800

    fix: change site title
    feat: redirect to home page if user has no name data nor roomId data

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
+import { useRecoilValue } from 'recoil';
 import Home from './views/Home';
 import WaitRoom from './views/WaitRoom';
 import GameRoom from './views/GameRoom';
 import InvitePage from './views/InvitePage';
+import { userRoomState, userNameState } from 'store/user';
 
 import {
   HashRouter as Router,
   Switch,
   Route,
-  // Redirect
+  Redirect
 } from "react-router-dom";
 
-const App = () => (
-    <>
+const App = () => {
+  const roomId = useRecoilValue(userRoomState);
+  const userName = useRecoilValue(userNameState);
+  
+  return (
     <Router>
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route path="/w/:roomId/:userID" component={WaitRoom} />
-        <Route path="/g/:roomId/:userID" component={GameRoom} />
+        <Route path="/w/:roomId/:userID">
+          {roomId && userName ? <WaitRoom /> : <Redirect to="/" />}
+        </Route>
+        <Route path="/g/:roomId/:userID">
+          {roomId && userName ? <GameRoom /> : <Redirect to="/" />}
+        </Route>
         <Route path="/i/:roomId" component={InvitePage} />
       </Switch>
     </Router>
-    </>
-)
+)};
 
 export default App;
